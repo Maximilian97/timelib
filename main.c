ac954dcb347fc0198979b5e261244678a5cdc3b5
Maximilian Hedderich <mhedderich@sievers-group.com>
2021-02-28 17:55:38 +0100

    Stable version of main.c

--- main.c
+++ main.c
@@ -1,24 +1,21 @@
 /**
- *
- *
- *
+ *  Program that outputs the days since the begin of the year
+ *  Author: Maximilian Hedderich
+ *  Created: 28.02.2021
  **/
 
+//Pre processor instruction to include libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include "functions.h"
+#define ResultMsg printf("Today is the %i. day of the year",
+#define End );
 
-/**
- *
- *
- **/
-int day_of_the_year(int day, int month, int year)
-{
-
-    return 0;
-}
-
+//main function
 int main()
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
-    return 0;
+    int day, month, year;
+
+    input_date(&day, &month, &year);
+    ResultMsg day_of_the_year(day, month, year) End
 }
